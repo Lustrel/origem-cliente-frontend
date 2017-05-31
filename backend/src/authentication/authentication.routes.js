90cf799029be8792ae90aaf1232763c69cc0b539
Stanley Sathler <stanleysathlerpinto@gmail.com>
2017-05-30 23:44:52 -0300

    Commiting some more codes

---
+++ backend/src/authentication/authentication.routes.js
@@ -0,0 +1,16 @@
+"use strict";
+
+(function(){
+
+	const AuthenticationController = require("./authentication.controller");
+
+	function AuthenticationRouter(router)
+	{
+		router.get("/authentication/login", (req, res, next) => 
+		{
+			
+		});
+	}
+
+})();
+
